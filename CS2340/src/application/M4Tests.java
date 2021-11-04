80f7880ba66419ca5bb558a033e7f2958fbda6f6
Ethan Haarer <ehaarer@stanford.edu>
2021-11-04 12:51:27 -0400

    Actual M4 Test File Add
    Ignore earlier push, this correctly adds the test file

---
+++ CS2340/src/application/M4Tests.java
@@ -0,0 +1,20 @@
+package application;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class M3Tests {
+    @Test
+    //This test tests an enemy’s conditions for death, either being that they have no more health or if they attack the monument they will be deleted. 
+    //This is important because we need to ensure that once the ability for towers to attack enemies is correctly implemented, 
+    //these enemies will die/be deleted given the correct commands to ensure that they function as intended.
+    public void ethanEnemyDeathTest() {
+    	
+    }
+    
+    @Test
+    public void ethanEnemyAttackMonumentTest() {
+    	
+    }
+
+}
