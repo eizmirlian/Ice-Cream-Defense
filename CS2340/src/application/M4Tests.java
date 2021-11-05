431b42a88b60928e35778f8e5cc99259094acf3c
brandonnoll <nollbrandonn@gmail.com>
2021-11-04 20:00:20 -0400

    Adding tests

--- CS2340/src/application/M4Tests.java
+++ CS2340/src/application/M4Tests.java
@@ -14,8 +14,10 @@ public class M4Tests {
     }
     
     @Test
-    //This test tests an enemy’s conditions for death, either being that they have no more health or if they attack the monument they will be deleted. 
-    //This is important because we need to ensure that once the ability for towers to attack enemies is correctly implemented, 
+    //This test tests an enemyï¿½s conditions for death, 
+    //either being that they have no more health or if they attack the monument they will be deleted. 
+    //This is important because we need to ensure 
+    //that once the ability for towers to attack enemies is correctly implemented, 
     //these enemies will die/be deleted given the correct commands to ensure that they function as intended.
     public void ethanEnemyAttackMonumentTest() {
     	Monument m1 = new Monument(100, 20);
@@ -71,5 +73,17 @@ public class M4Tests {
     	testEnemy.setPos(testArr);
     	testEnemy.takeDamage(1);
     	Assert.assertArrayEquals(testEnemy.getPos(), testArr, 0);
+    }
+
+    @Test 
+    public void brandonBuyTowerTest() {
+        Tower tower = new Tower(Difficulty.EASY, "TEST TOWER", 40, 40, 40, 40);
+        assertEquals(tower.getDescription, "TEST TOWER");
+    }
+
+    @Test
+    public void brandonTowerConstructorTest() {
+        Tower tower = new Tower(Difficulty.EASY, "TEST TOWER", 40, 40, 40, 40);
+        assertEquals(1, 1);
     }
 }
