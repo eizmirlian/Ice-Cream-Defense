bb0925ac6beb03718f6524e8e4cd9172fd9caf57
Ethan Haarer <ehaarer@stanford.edu>
2021-11-19 10:37:20 -0500

    Add M5 Test File
    Add M5 Test File to applications folder to begin implementing tests

---
+++ CS2340/src/application/M5Tests.java
@@ -0,0 +1,52 @@
+package application;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class M5Tests {
+    @Test
+    public void ethanEnemyDeathTest() {
+        
+    }
+    
+    @Test
+    public void eliasSpeedDiffTest() {
+        
+    }
+    
+    @Test
+    public void eliasDiffMoneyOnEnemyDeathTest() {
+        
+    }
+    
+    @Test
+    public void jgEnemyDamageTest() {
+        
+    }
+    
+    @Test
+    public void jgEnemyDiffDamageTest() {
+        
+    }
+    
+    @Test
+    public void ethanOnDeathAddMoneyTest() {
+        
+    }
+    
+    @Test
+    public void ethanTestDifferentTowerAttributes() {
+        
+    }
+    
+    @Test
+    public void enemyDamageTest() {
+        
+    }
+    
+    @Test
+    public void towerIsAliveTest() {
+        
+    }
+}
