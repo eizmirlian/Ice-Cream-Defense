751b82def03e789acbfe54bf8c7ac04fce6b50cd
Ethan Haarer <ehaarer@stanford.edu>
2021-12-07 18:57:36 -0500

    Checkstyle fix
    Fixed some checkstyle errors

--- CS2340/src/application/M6Tests.java
+++ CS2340/src/application/M6Tests.java
@@ -5,27 +5,27 @@ import org.junit.jupiter.api.Test;
 public class M6Tests {
     @Test
     public void jgIsUpgradedTest() {
-    	Tower testTower = new VanillaCatapult(Difficulty.EASY);
-    	assertEquals(testTower.checkUpgrade(), false);
-    	testTower.upgrade();
-    	assertEquals(testTower.checkUpgrade(), true);
+        Tower testTower = new VanillaCatapult(Difficulty.EASY);
+        assertEquals(testTower.checkUpgrade(), false);
+        testTower.upgrade();
+        assertEquals(testTower.checkUpgrade(), true);
     }
 
     
     @Test
     public void jgUpgradePriceTest() {
-    	Tower testTower = new VanillaCatapult(Difficulty.EASY);
-    	double price = testTower.getPrice();
-    	assertEquals(testTower.getUpgradePrice(), price * 3/4);
+        Tower testTower = new VanillaCatapult(Difficulty.EASY);
+        double price = testTower.getPrice();
+        assertEquals(testTower.getUpgradePrice(), price * 3 / 4);
     }    
     
     @Test
     public void eliasSpeedDiffTest() {
         Path p1 = new Path(0, 0, 0, 0);
-    	Enemy finBoss = new FinalBoss(p1, false);
-    	assertEquals(finBoss.getHealth(), 800);
+        Enemy finBoss = new FinalBoss(p1, false);
+        assertEquals(finBoss.getHealth(), 800);
     }
-	
+    
     @Test
     public void eliasDamageFinalBossTest() {
         Path p1 = new Path(0, 0, 0, 0);
@@ -65,7 +65,7 @@ public class M6Tests {
         assertEquals(t3.getFireRate(), 1);
     }
 
-	@Test
+    @Test
     public void ethanGoToWinScreenTest() {
         Path p1 = new Path(0, 0, 0, 0);
         p1.setLastTile(true);
@@ -99,5 +99,5 @@ public class M6Tests {
         Path p1 = new Path(0, 0, 0, 0);
         assertEquals(true, true);
     }
-	
+    
 }
